3df6d66191e94dd057a045b504334ea1576e520d
Sebastien Wertz <sebastien.wertz@cern.ch>
2021-09-14 14:43:52 +0200

    retrieve crab scripts from topNanoV6

---
+++ PhysicsTools/NanoAOD/test/README.md
@@ -0,0 +1,87 @@
+# TopNanoAOD
+
+## Recipe: setting up (v9-1-X)
+
+Based on central nanoAOD v9, CMSSW_10_6_27. On CC7/SLC7:
+```bash
+export SCRAM_ARCH=slc7_amd64_gcc700
+cmsrel CMSSW_10_6_27
+cd CMSSW_10_6_27/src/
+cmsenv
+git cms-init
+git cms-checkout-topic cms-top:topNanoV9-1-1_10_6_27
+scram b -j 8
+```
+
+## Submitting crab jobs
+
+```
+voms-proxy-init --rfc --voms cms --valid 96:00
+source /cvmfs/cms.cern.ch/common/crab-setup.sh # or .csh
+cd $CMSSW_BASE/src/PhysicsTools/NanoAOD/test
+```
+
+You'll need to prepare a YAML file listing all the samples you want to process. An example is given in [test/topSamples.yml](./topSamples.yml). You can either use the name of the miniAOD dataset you want to process, or the name of the nanoAOD dataset you want to reproduce (the script will then automatically fetch the parent miniAOD dataset). Then, run:
+```
+./runTopNanoOnGrid.py -d YOURLIST.yml -o crab_tasks -s YOURSITE
+```
+Replace `YOURSITE` by a T2/T3 site at which you have write permissions. This creates the crab config files in the `crab_tasks` folder, but doesnt submit any jobs. You can then `cd crab_tasks` and submit all or some of them using `crab submit PY_CFG`.
+
+After the processing is complete, you can retrieve the output dataset name using `crab status TASK_DIR`, and add it to the repository holding all the topNanoAOD datasets: https://github.com/cms-top/topNanoAOD-datasets/
+
+**Important**: the Crab files included here are configured in such a way that the output datasets will be known to [Rucio](https://twiki.cern.ch/twiki/bin/view/CMS/Rucio), the new sample management tool, so that people are able to request copies of the topNanoAOD outputs to their local computing centre using Rucio.
+
+## Troubleshooting
+
+You might need to adjust the job splitting options if jobs take too long to run (or run too quickly). Resubmitting using `crab resubmit --maxjobruntime` and `--siteblacklist` can also help.
+
+In case you accidentally published a dataset twice under the same publication name, one can invalidate specific files of the dataset:
+```
+wget -O DBS3SetFileStatus.py https://twiki.cern.ch/twiki/pub/CMSPublic/Crab3DataHandling/DBS3SetFileStatus.py.txt
+python DBS3SetFileStatus.py --url=https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter --status=invalid --recursive=False  --files=<LFN>
+```
+To invalidate a complete dataset, use this command:
+```
+wget -O DBS3SetDatasetStatus.py https://twiki.cern.ch/twiki/pub/CMSPublic/Crab3DataHandling/DBS3SetDatasetStatus.py.txt
+python DBS3SetDatasetStatus.py --dataset=<datasetname> --url=https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter --status=INVALID --recursive=False
+```
+More information about publication [here](https://twiki.cern.ch/twiki/bin/view/CMSPublic/Crab3DataHandling#Changing_a_dataset_or_file_statu).
+
+## Version history
+
+### nanoAODv9
+
+- **v9-1-1**:
+    - Store full set of PS weights
+    - Origin of B and C hadrons ghost-matched to genJets
+    - Crab submission scripts: enable Rucio publication of outputs
+    - [Auto-generated documentation](https://swertz.web.cern.ch/swertz/TMG/TopNano/TopNanoV9-1-1/doc_topNanoV9-1-1.html)
+    - [Full code changes w.r.t central nanoAOD](https://github.com/cms-top/cmssw/compare/CMSSW_10_6_27...topNanoV9-1-1_10_6_27)
+
+### nanoAODv6
+
+- **v6-1-2**:
+    - Note: no event content change w.r.t v6p1
+    - Fix DBS publication for data
+- **v6-1-1**:
+    - Note: no event content change w.r.t v6p1
+    - Add cmsDriver cfg's and adapt crab submission scripts for also running on data
+    - Fix typo in README
+    - [Full code changes](https://github.com/cms-top/cmssw/compare/CMSSW_10_2_18...topNanoV6-1-1_10_2_18)
+- **v6p1**: see detailed report [here](https://indico.cern.ch/event/921985/contributions/3873532/attachments/2043975/3423837/200525_topNanoV6p1.pdf)
+    - TOP lepton MVA
+    - Store NNPDF3.1 weights when available (instead of PDF4LHC)
+    - PS weights storage fixed as in [this PR](https://github.com/cms-nanoAOD/cmssw/pull/506)
+    - Origin of B and C hadron ghost-matched to genJets
+    - [Auto-generated documentation](https://swertz.web.cern.ch/swertz/TMG/TopNano/TopNanoV6p1/doc_topNanoV6p1.html)
+    - [Size report](https://swertz.web.cern.ch/swertz/TMG/TopNano/TopNanoV6p1/report_topNanoV6p1.html)
+    - [Full code changes](https://github.com/cms-top/cmssw/compare/CMSSW_10_2_18...topNanoV6p1_10_2_18)
+
+
+### nanoAODv5
+
+See instructions [here](https://github.com/demuller/privNanoAOD).
+
+## Contributing changes
+
+If you'd like to propose some changes for future iterations, please open an [issue](https://github.com/cms-top/cmssw/issues) to discuss them, and then open a pull request towards the `topNanoV9_from-CMSSW_10_6_27` branch.
---
+++ PhysicsTools/NanoAOD/test/runTopNanoOnGrid.py
@@ -0,0 +1,175 @@
+#! /usr/bin/env python
+
+from CRABAPI.RawCommand import crabCommand
+import CRABClient.UserUtilities as crab
+
+import json
+import copy
+import os
+import argparse
+import sys
+
+# DAS client
+from Utilities.General.cmssw_das_client import get_data as myDASclient
+
+CMSSW_ROOT = os.path.join(os.environ['CMSSW_BASE'], 'src')
+NANO_ROOT = os.path.join(os.environ['CMSSW_BASE'], 'src', 'PhysicsTools', 'NanoAOD')
+PROD_TAG = "v6-1-2"
+
+def retry(nattempts, exception=None):
+    """
+    Decorator allowing to retry an action several times before giving up.
+    @params:
+        nattempts  - Required: maximal number of attempts (Int)
+        exception  - Optional: if given, only catch this exception, otherwise catch 'em all (Exception)
+    """
+
+    def tryIt(func):
+        def wrapper(*args, **kwargs):
+            attempts = 0
+            while attempts < nattempts - 1:
+                try:
+                    return func(*args, **kwargs)
+                except (exception if exception is not None else Exception):
+                    attempts += 1
+            return func(*args, **kwargs)
+        return wrapper
+    return tryIt
+
+@retry(5)
+def get_parent_DAS(dataset):
+    """Retrieve parent dataset from DAS"""
+    data = myDASclient("parent dataset=" + dataset)
+    if data['status'] != 'ok':
+        raise Exception("Failed retrieving parent dataset from DAS.\n{}".format(data))
+    assert(len(data['data']) == 1)
+    assert(len(data['data'][0]['parent']) == 1)
+    return data['data'][0]['parent'][0]['name']
+
+
+def get_options():
+    """
+    Parse and return the arguments provided by the user.
+    """
+    parser = argparse.ArgumentParser(description='Generate crab config files for multiple datasets.')
+
+    parser.add_argument('-e', '--era', required=False, choices=['2016', '2017', '2018', '2018ABC', '2018D'], help='Choose specific era. If not specified, run on all eras')
+    parser.add_argument('-d', '--datasets', nargs='*', help='Json file(s) with dataset list')
+    parser.add_argument('-s', '--site', required=True, help='Site to which to write the output')
+    parser.add_argument('-o', '--output', default='./', help='Folder in which to write the config files')
+
+    return parser.parse_args()
+
+
+def create_default_config(is_mc):
+    config = crab.config()
+
+    config.General.workArea = 'tasks'
+    config.General.transferOutputs = True
+    config.General.transferLogs = True
+    config.JobType.allowUndistributedCMSSW = True # for slc7
+
+    config.JobType.pluginName = 'Analysis'
+    config.JobType.maxMemoryMB = 5000
+    config.JobType.numCores = 2
+
+    config.Data.inputDBS = 'global'
+    config.Data.publication = True
+
+    if is_mc:
+        config.Data.splitting = 'EventAwareLumiBased'
+        config.Data.unitsPerJob = 280000
+    else:
+        config.Data.splitting = 'LumiBased'
+        config.Data.unitsPerJob = 220
+
+    return config
+
+
+def findPSet(pset):
+    c = pset
+    if not os.path.isfile(c):
+        # Try to find the psetName file
+        filename = os.path.basename(c)
+        path = NANO_ROOT
+        c = None
+        for root, dirs, files in os.walk(path):
+            if filename in files:
+                c = os.path.join(root, filename)
+                break
+        if c is None:
+            raise IOError('Configuration file %r not found' % filename)
+    return os.path.abspath(c)
+
+
+def writeCrabConfig(pset, dataset, is_mc, metadata, era, crab_config, site, output):
+    c = copy.deepcopy(crab_config)
+
+    c.JobType.psetName = pset
+
+    name = metadata.pop('name')
+
+    c.General.requestName = "TopNanoAOD{}_{}__{}".format(PROD_TAG, name, era)
+
+    c.Data.outputDatasetTag = "TopNanoAOD{}_{}".format(PROD_TAG, era)
+    c.Data.inputDataset = dataset
+    c.Data.outLFNDirBase = '/store/user/{user}/topNanoAOD/{tag}/{era}/'.format(user=os.getenv('USER'), tag=PROD_TAG, era=era)
+    c.Site.storageSite = site
+
+    # customize if asked
+    for attr,val in metadata.items():
+        setattr(getattr(c, attr.split(".")[0]), attr.split(".")[1], val)
+
+    print("Creating new task {}".format(c.General.requestName))
+
+    # Create output file
+    crab_config_file = os.path.join(output, 'crab_' + c.General.requestName + '.py')
+    with open(crab_config_file, 'w') as f:
+        f.write(str(c))
+
+    print('Configuration file saved as %r' % (crab_config_file))
+
+
+
+if __name__ == "__main__":
+
+    options = get_options()
+    
+    if not os.path.isdir(options.output):
+        os.makedirs(options.output)
+
+    # Load datasets
+    datasets = {}
+    for dataset in options.datasets:
+        with open(dataset) as f:
+            datasets.update(json.load(f))
+
+    crab_config_mc = create_default_config(True)
+    crab_config_data = create_default_config(False)
+
+    for era, era_datasets in datasets.items():
+        if options.era and era != options.era:
+            continue
+
+        for dataset, metadata in era_datasets.items():
+            print("Working on {}".format(dataset))
+            if dataset.endswith("NANOAODSIM") or dataset.endswith("NANOAOD"):
+                print("Will convert from nano to mini!")
+                dataset = get_parent_DAS(dataset)
+                print(" --> Found {}".format(dataset))
+            elif (not (dataset.endswith("MINIAODSIM") or dataset.endswith("MINIAOD"))) and not dataset.endswith("USER"):
+                print("Dataset {} cannot be used - must be either nano or mini!".format(dataset))
+
+            is_mc = dataset.endswith("SIM")
+            if is_mc:
+                print("Dataset is MC")
+                pset = findPSet("topNano_{}_{}_MC_cfg.py".format(PROD_TAG, era))
+                crab_config = crab_config_mc
+            else:
+                print("Dataset is data")
+                crab_config = crab_config_data
+                pset = findPSet("topNano_{}_{}_data_cfg.py".format(PROD_TAG, era))
+
+            year = "".join(i for i in era if i.isdigit()) # just keep the year from now on
+            writeCrabConfig(pset, dataset, is_mc, metadata, year, crab_config, options.site, options.output)
+            print("")
